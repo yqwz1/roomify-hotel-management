1732ee01b7552184d683216222c2d16100e411eb
wahib <wahibpop@gmail.com>
2026-01-18 11:26:31 +0300

    chore: fix eslint for JSX usage

--- frontend/eslint.config.js
+++ frontend/eslint.config.js
@@ -1,13 +1,14 @@
 import js from '@eslint/js'
 import globals from 'globals'
+import react from 'eslint-plugin-react'
 import reactHooks from 'eslint-plugin-react-hooks'
 import reactRefresh from 'eslint-plugin-react-refresh'
 
 export default [
-  {
-    ignores: ['dist/**', 'node_modules/**', 'coverage/**'],
-  },
+  { ignores: ['dist/**', 'node_modules/**', 'coverage/**'] },
+
   js.configs.recommended,
+
   {
     files: ['**/*.js', '**/*.jsx'],
     languageOptions: {
@@ -20,11 +21,22 @@ export default [
         ...globals.browser,
       },
     },
+    settings: {
+      react: { version: 'detect' },
+    },
     plugins: {
+      react,
       'react-hooks': reactHooks,
       'react-refresh': reactRefresh,
     },
     rules: {
+      // This fixes: "'App' is defined but never used" when used in JSX
+      'react/jsx-uses-vars': 'error',
+
+      // New JSX transform (React 17+) doesn't require React in scope
+      'react/react-in-jsx-scope': 'off',
+      'react/jsx-uses-react': 'off',
+
       ...reactHooks.configs.recommended.rules,
       'react-refresh/only-export-components': ['warn', { allowConstantExport: true }],
     },
