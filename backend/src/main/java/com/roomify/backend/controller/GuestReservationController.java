61a08f9938169a5c7311c01f4071ec35d410e792
wahib <wahibpop@gmail.com>
2026-04-18 22:39:03 +0300

    fixed the big shit
    big shit got fixed yay

--- backend/src/main/java/com/roomify/backend/controller/GuestReservationController.java
+++ backend/src/main/java/com/roomify/backend/controller/GuestReservationController.java
@@ -4,7 +4,6 @@ import com.roomify.backend.dto.GuestReservationsResponse;
 import com.roomify.backend.service.GuestReservationService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -12,7 +11,6 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/api/guest/reservations")
 @RequiredArgsConstructor
-@PreAuthorize("hasRole('GUEST')")
 public class GuestReservationController {
 
     private final GuestReservationService guestReservationService;
